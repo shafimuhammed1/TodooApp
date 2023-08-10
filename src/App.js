c4ffa5dfe0670fc5c87b4c95e98e4f3e617856b4
Muhammed Shafi <mohdshafi13112000@gmail.com>
2023-08-10 22:55:18 +0530

    todoo app completed

--- src/App.js
+++ src/App.js
@@ -16,18 +16,37 @@ function App() {
       </div>
       <div className="input">
         <input value={Todo} onChange={(event)=>setTodo(event.target.value)}   type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <i onClick={()=>setTodos([...Todos,{id:Date.now() ,text:Todo,status:false}])} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-        <div className="todo">
+      {  Todos.map((obj)=>{
+
+      
+      return(<div className="todo">
           <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
+            <input onChange={(event)=>{
+              setTodos(Todos.filter(obj2=>{
+                if(obj2.id===obj.id){
+                  obj2.status=event.target.checked
+                }
+                return obj2
+              }))
+
+            }} value={obj.text} type="checkbox" name="" id="" />
+            <p>{obj.text}</p>
           </div>
           <div className="right">
             <i className="fas fa-times"></i>
           </div>
-        </div>
+        </div>)
+        })}
+
+        {Todos.map((obj)=>{
+          if(obj.status){
+            return(<h1>{obj.text}</h1>)
+          }
+          return null
+        })}
       </div>
     </div>
   )
